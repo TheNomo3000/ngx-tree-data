20579ca3dffd72f5b9b71afc4713fb9408b46042
Rodrigo <rodrigo.zevallos@studentpartner.com>
2019-08-05 22:07:18 +0200

    Use new data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,42 +10,11 @@ import { DATA } from './data/data';
 })
 export class AppComponent {
   @Output() selectedChange = new EventEmitter<ItemNode>();
+  checkbox = true;
+  search = true;
   data = DATA;
   dataSelected;
   constructor(private treeService: NgxTreeDataService) {
-    const dataSource = [];
-    let parent = 1;
-    let children = 1;
-    this.data.forEach( item => {
-      dataSource.push(
-        {
-          id: item.id,
-          text: item.name,
-          code: `0.${parent}`,
-          data: {
-            parent: true,
-          }
-        }
-      );
-      const childrens = item.childrens;
-      if ( childrens.length > 0) {
-        childrens.forEach( subitem => {
-          dataSource.push(
-            {
-              id: subitem.id,
-              text : subitem.name,
-              code : `0.${parent}.${children}`,
-              parent: item.name,
-              data : {
-                parent: false,
-              }
-            }
-          );
-          children++;
-        });
-      }
-      parent++;
-    });
-    this.treeService.initialize(dataSource);
+    this.treeService.initialize(this.data);
   }
 }
